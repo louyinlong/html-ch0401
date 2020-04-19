7d4706224ae2aba24807d6ce1b971ae4bd32532c
louyinlong <1092755001@qq.com>
2020-04-19 13:30:20 +0800

    ch0401
    Signed-off-by: louyinlong <1092755001@qq.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TimeoutError } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,44 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ch0401';
+
+  shouldShow = false;
+
+  score = 70;
+
+  color: string;
+
+  fontSize: string;
+
+  isBordered = true;
+
+  classObj = {
+    bordered: this.isBordered,
+    blue: false,
+    round: true
+  }
+
+  onToggle() {
+    this.shouldShow = !this.shouldShow;
+    return false;
+  }
+
+  grade() {
+    return Math.floor(this.score / 10);
+  }
+
+  onInput(evt: Event) {
+    const inputEle = <HTMLInputElement>evt.target;
+    this.score = Number(inputEle.value);
+  }
+
+  apply(color: string, fontSize: string) {
+    this.color = color;
+    this.fontSize = fontSize;
+  }
+
+  toggleBorder() {
+    this.isBordered = !this.isBordered;
+  }
+
 }
